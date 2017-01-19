4a9ff4c8aa1f5a9c6128a56ffd6d7c06655e5624
Astyan <astyan@omnos.fr>
2017-01-19 08:17:18 +0100

    migrations working

--- web/djangominimal/settings.py
+++ web/djangominimal/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'djangominimal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_SERVICE'],
-        'PORT': os.environ['DB_PORT'] 
+        'NAME': os.environ['POSTGRES_NAME'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_SERVICE'],
+        'PORT': os.environ['POSTGRES_PORT'] 
     }
 }
 
