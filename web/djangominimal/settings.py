12e054ffb5d7880df3ed5019e904ab435ea95633
Astyan <astyan@omnos.fr>
2017-01-19 12:19:40 +0100

    changing a little the setup

--- web/djangominimal/settings.py
+++ web/djangominimal/settings.py
@@ -73,18 +73,24 @@ WSGI_APPLICATION = 'djangominimal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['POSTGRES_NAME'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': os.environ['POSTGRES_SERVICE'],
-        'PORT': os.environ['POSTGRES_PORT'] 
+if 'POSTGRES_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['POSTGRES_NAME'],
+            'USER': os.environ['POSTGRES_USER'],
+            'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+            'HOST': os.environ['POSTGRES_SERVICE'],
+            'PORT': os.environ['POSTGRES_PORT'] 
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
