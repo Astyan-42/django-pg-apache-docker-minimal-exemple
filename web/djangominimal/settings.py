a42b5d96e09593f9bee6d7f05aa0042ccd376181
Astyan <astyan@omnos.fr>
2017-01-21 21:42:38 +0100

    adding named volumes

--- web/djangominimal/settings.py
+++ web/djangominimal/settings.py
@@ -77,7 +77,7 @@ if 'POSTGRES_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['POSTGRES_NAME'],
+            'NAME': os.environ['POSTGRES_DB'],
             'USER': os.environ['POSTGRES_USER'],
             'PASSWORD': os.environ['POSTGRES_PASSWORD'],
             'HOST': os.environ['POSTGRES_SERVICE'],
